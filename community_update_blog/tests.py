ff8f219ef06eb274a067895279ecd52ff9b59669
Sean <seanpatmeade@gmail.com>
2022-08-16 21:55:44 +0100

    Started on automated tests for Community updates

--- community_update_blog/tests.py
+++ community_update_blog/tests.py
@@ -1,3 +1,39 @@
-from django.test import TestCase
+from unittest import TestCase
+from django.test import Client
+from .forms import CommentForm
+from .views import CommunityUpdatesMostRecent
 
-# Create your tests here.
+class TestCommentForm(TestCase):
+
+    def test_empty_comment(self):
+        form = CommentForm({'body': ''})
+        self.assertFalse(form.is_valid())
+
+    def test_comment(self):
+        form = CommentForm({'body': 'hello I\'m a comment!'})
+        self.assertTrue(form.is_valid())
+
+
+class TestIndexMostRecent(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+
+    def test_most_recent(self):
+        response = self.client.get('')
+        self.assertIsNotNone(response)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('Ogonnelloe', str(response.content))
+        self.assertIn('recent_posts', response.context)
+
+# TODO: class TestDetailedView()
+
+class TestCommunityUpdateLike(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+
+    def test_like(self):
+        response = self.client.post('like/2')
+        self.assertIsNotNone(response)
+        self.assertEqual(response.status_code, 200)
--- community_website_ogonnelloe/settings.py
+++ community_website_ogonnelloe/settings.py
@@ -108,9 +108,24 @@ WSGI_APPLICATION = 'community_website_ogonnelloe.wsgi.application'
 #     }
 # }
 
-DATABASES = {
-    'default': dj_database_url.parse(DATABASE_URL)
-}
+# DATABASES = {
+#         'ENGINE': dj_database_url.parse(DATABASE_URL),
+#         'NAME': '',
+# }
+
+if 'DATABASE_URL' in os.environ:
+    print("Postgres URL found, using it as db")
+    DATABASES = {
+        "default": dj_database_url.parse(DATABASE_URL)
+    }
+else:
+    print("Postgres URL not found, using sqlite3 instead")
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
